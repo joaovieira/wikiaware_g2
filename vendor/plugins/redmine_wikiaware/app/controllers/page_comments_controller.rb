18af8656a73d513eb4ff4eec9f5c0ae977667b5e
Joao Vieira <shocker@abandonados.info>
2010-12-02 21:31:07 +0000

    Comentarios bonitos e remocao

--- vendor/plugins/redmine_wikiaware/app/controllers/page_comments_controller.rb
+++ vendor/plugins/redmine_wikiaware/app/controllers/page_comments_controller.rb
@@ -4,10 +4,9 @@ class PageCommentsController < ApplicationController
 
   def create
   	@page = WikiPage.find(params[:wiki_page_id])
-  	@comment = PageComment.new(params[:page_comment])
+  	@comment = PageComment.new(:content => params[:content_text], :version_id => @page.content.version)
   	@comment.user = User.current
   	@comment.wiki_page = @page
-  	@comment.version_id = @page.content.version
   	
   	if @comment.save
   	  flash[:notice] = "Comment was successfully submited."
@@ -16,6 +15,17 @@ class PageCommentsController < ApplicationController
   	end
   	
   	redirect_to :back
+  end
+  
+  def destroy
+    @comment = PageComment.find(params[:id])
+    if @comment.destroy
+  	  flash[:notice] = "Comment deleted."
+  	else
+  	  flash[:error] = "Error deleting comment!"
+  	end
+  	
+    redirect_to :back
   end
 
 end
